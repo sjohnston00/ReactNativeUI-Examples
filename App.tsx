97b09c573d241be5cf259e398271317f95a99294
Sam Jonston <johnstonsam712@gmail.com>
2022-02-05 22:43:14 +0000

    Adding react-native-ui-lib though EXPO

--- App.tsx
+++ App.tsx
@@ -1,20 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import "react-native-gesture-handler";
+import { StatusBar } from "expo-status-bar";
+import { Text, View } from "react-native-ui-lib";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View flex paddingT-120>
+      <Text green20 text20>
+        Testing app!
+      </Text>
+      <StatusBar style='auto' />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
